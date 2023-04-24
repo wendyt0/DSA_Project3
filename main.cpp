c7b217941c4422a116188294e07be9c19a233170
wendyt0 <100869311+wendyt0@users.noreply.github.com>
2023-04-24 19:18:08 -0400

    main menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int command;
+    cout << "Welcome to BnBeyond!" << endl;
+    cout << "Main Menu:" << endl;
+    cout << "0. Sort by price" << endl;
+    cout << "1. Sort by rating" << endl;
+    cout << "2. Sort by capacity" << endl;
+    cout << "3. Sort by cleanliness" << endl;
+    cout << "4. Sort by distance to city center" << endl;
+    cout << "5. Sort by distance to nearest metro" << endl;\
+    cout << "6. Exit" << endl << endl;
+    cout << "Please enter a command: ";
+    cin >> command;
+
     return 0;
 }
-
-//test edit push
-// this is a test too
