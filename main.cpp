7ea55652030d2edf6cd7fd8c507eb1fd775e2578
wendyt0 <100869311+wendyt0@users.noreply.github.com>
2023-04-24 19:37:35 -0400

    main menu updated, gets filename

--- main.cpp
+++ main.cpp
@@ -2,18 +2,38 @@
 using namespace std;
 
 int main() {
+//    int command;
+    string location;
+    string week;
+    string filename;
     int command;
     cout << "Welcome to BnBeyond!" << endl;
-    cout << "Main Menu:" << endl;
-    cout << "0. Sort by price" << endl;
-    cout << "1. Sort by rating" << endl;
-    cout << "2. Sort by capacity" << endl;
-    cout << "3. Sort by cleanliness" << endl;
-    cout << "4. Sort by distance to city center" << endl;
-    cout << "5. Sort by distance to nearest metro" << endl;\
-    cout << "6. Exit" << endl << endl;
-    cout << "Please enter a command: ";
-    cin >> command;
+    cout << "Where are you traveling to? ";
+    cin >> location;
+    if(location == "amsterdam" || location == "athens" || location == "barcelona" || location == "berlin" || location == "budapest"
+    || location == "lisbon" || location == "london" || location == "paris" || location == "rome" || location == "vienna") {
+        cout << "Do you plan on staying during a weekend or a weekday? ";
+        cin >> week;
+        if(week == "weekend" || week == "weekday") {
+            filename = location + "_" + week + ".csv";
+            cout << endl << "Main Menu:" << endl;
+            cout << "0. Sort by price" << endl;
+            cout << "1. Sort by rating" << endl;
+            cout << "2. Sort by capacity" << endl;
+            cout << "3. Sort by cleanliness" << endl;
+            cout << "4. Sort by distance to city center" << endl;
+            cout << "5. Sort by distance to nearest metro" << endl;\
+            cout << "6. Exit" << endl;
+            cout << "Please enter a command: ";
+            cin >> command;
+        }
+        else {
+            cout << "Invalid entry, please try again.";
+        }
+    }
+    else {
+        cout << "Sorry, we do not support that location at this time.";
+    }
 
     return 0;
 }
